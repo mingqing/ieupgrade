d2e61994567d4b20ddbf8d1c42728ed1a770f959
mingqing <mingqing@henji.org>
2016-04-10 12:03:28 +0800

    update

--- browser.js
+++ browser.js
@@ -1,14 +1,11 @@
-var isIE=!!window.ActiveXObject;
-var isIE6=isIE&&!window.XMLHttpRequest;
-var isIE8=isIE&&!!document.documentMode;
-var isIE7=isIE&&!isIE6&&!isIE8;
-
-if (isIE){
-  if (isIE6){
-    alert("ie6");
-  } else if (isIE7){
-    alert("ie7");
-  } else if (isIE8){
-    alert("ie8");
-  }
+var isIE = function(ver){
+  var b = document.createElement('b')
+  b.innerHTML = '<!--[if IE ' + ver + ']><i></i><![endif]-->'
+  return b.getElementsByTagName('i').length === 1
+}
+if(isIE(6)){
+}
+if(isIE(7)){
+}
+if(isIE(8)){
 }
